29fc3e0535a9613f3d094668897c4e9a206c9d04
ndbzika <chakcpps@gmail.com>
2023-10-10 09:44:15 -0300

    feat: Tratamento de erros #6

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
+import 'express-async-errors';
 import express from 'express';
 import dotenv from 'dotenv';
+import { errorMiddleware } from './middlewares/error';
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(errorMiddleware);
 
 export default app;
