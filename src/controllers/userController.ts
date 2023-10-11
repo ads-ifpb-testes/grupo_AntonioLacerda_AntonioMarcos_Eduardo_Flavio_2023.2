3219b16c281ae01d31a1f2d37957acddb36e85b1
ndbzika <chakcpps@gmail.com>
2023-10-10 23:43:08 -0300

    feat: user routes #7

---
+++ src/controllers/userController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import {
+  createUser,
+  deleteUser,
+  findUser,
+  updateUser
+} from '../services/userServices';
+
+const show = async (req: Request, res: Response) => {
+  const { email } = req.params;
+  const user = await findUser(email);
+  return res.send(user);
+};
+
+const create = async (req: Request, res: Response) => {
+  const user = req.body;
+  const createdUser = await createUser(user);
+  return res.status(201).send(createdUser);
+};
+
+const update = (req: Request, res: Response) => {
+  const { email } = req.params;
+  const userData = req.body;
+  const updatedUser = updateUser(email, userData);
+  return res.status(204).send(updatedUser);
+};
+
+const destroy = (req: Request, res: Response) => {
+  const { email } = req.params;
+  deleteUser(email);
+  return res.status(204).send();
+};
+
+export const UserController = {
+  show,
+  create,
+  update,
+  destroy
+};
---
+++ src/services/userServices.ts
@@ -0,0 +1,103 @@
+import { PrismaClient } from '@prisma/client';
+import { UserDTO } from '../dtos/UserDTO';
+import {
+  BadRequestError,
+  InternalServerError,
+  NotFoundError
+} from '../helpers/api-errors';
+import { hash } from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+const hashPassword = async (password: string) => {
+  return await hash(password, 10);
+};
+
+const findUser = async (email: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  });
+  if (!user) {
+    throw new BadRequestError('User Not Found');
+  }
+  const { password, ...userWithoutPassword } = user;
+  return userWithoutPassword;
+};
+
+const createUser = async (user: UserDTO) => {
+  const userExists = await prisma.user.findUnique({
+    where: {
+      email: user.email
+    }
+  });
+  if (userExists) {
+    throw new BadRequestError('User already exists');
+  }
+  const createdUser = await prisma.user.create({
+    data: {
+      email: user.email,
+      name: user.name,
+      password: await hashPassword(user.password),
+      birthDate: user.birthDate,
+      country: user.country,
+      city: user.city,
+      address: user.adress,
+      phone: user.phone,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
+    }
+  });
+  if (!createdUser) {
+    throw new InternalServerError('Error creating user');
+  }
+  const { password, ...userWithoutPassword } = createdUser;
+  return userWithoutPassword;
+};
+
+const updateUser = async (email: string, userData: Partial<UserDTO>) => {
+  const user = await findUser(email);
+  if (!user) {
+    throw new BadRequestError('User not found');
+  }
+  const updatedUser = await prisma.user.update({
+    where: {
+      email
+    },
+    data: {
+      name: userData.name || user.name,
+      birthDate: userData.birthDate || user.birthDate,
+      country: userData.country || user.country,
+      city: userData.city || user.city,
+      address: userData.adress || user.address,
+      phone: userData.phone || user.phone,
+      updatedAt: new Date()
+    }
+  });
+
+  if (!updatedUser) {
+    throw new InternalServerError('Error updating user');
+  }
+  return;
+};
+
+const deleteUser = async (email: string) => {
+  const user = await findUser(email);
+  if (!user) {
+    throw new NotFoundError('User not found');
+  }
+  const deletedUser = await prisma.user.delete({
+    where: {
+      email
+    }
+  });
+
+  if (!deletedUser) {
+    throw new InternalServerError('Error deleting user');
+  }
+
+  return;
+};
+
+export { findUser, createUser, updateUser, deleteUser };
