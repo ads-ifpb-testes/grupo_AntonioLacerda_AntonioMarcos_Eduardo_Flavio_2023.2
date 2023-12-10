ca90bdca22f8f34aeb720f65e1d91b4fe04e68d6
ndbzika <chakcpps@gmail.com>
2023-12-10 16:58:30 -0300

    view: loading spinner

--- Front-End/js/index.js
+++ Front-End/js/index.js
@@ -2,19 +2,38 @@
 import { login, register } from './login.js';
 import { isTokenValid } from './scripts.js';
 
+const loadSpinner = document.createElement('div');
+loadSpinner.className = 'lds-ring';
+loadSpinner.innerHTML = `
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+`;
+
 window.onload = async () => {
   if (await isTokenValid()) {
     window.location.href = './html/home.html';
   }
 };
-
+let isLoading = false;
 const loginButton = document.getElementById('signin');
 
 loginButton?.addEventListener('click', async (e) => {
   e.preventDefault();
   const email = document.getElementById('email').value;
   const password = document.getElementById('senha').value;
-  await login(email, password);
+  isLoading = true;
+  loginButton.replaceChildren(loadSpinner);
+  await login(email, password)
+    .then(() => {
+      isLoading = false;
+      loginButton.replaceChildren('Entrar');
+    })
+    .catch(() => {
+      isLoading = false;
+      loginButton.replaceChildren('Entrar');
+    });
 });
 
 const registerButton = document.getElementById('signup');
